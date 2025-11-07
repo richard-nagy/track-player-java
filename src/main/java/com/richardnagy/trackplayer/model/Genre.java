a9e3f4de7277e29c74715a69b2568f0be05709eb
richard-nagy <big.ricsoo@gmail.com>
2025-11-07 11:31:40 +0100

    Add genre class

---
+++ src/main/java/com/richardnagy/trackplayer/model/Genre.java
@@ -0,0 +1,11 @@
+package com.richardnagy.trackplayer.model;
+
+public class Genre {
+    private int id;
+    private String name;
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
